e0d8eff9c482519a3731af9d176ee58b06e1b73b
shrutidhungana <shrutidhungana123@gmail.com>
2024-10-29 13:22:39 +0545

    Created authorization for different views 1)If user is not authenticated 2)If user is authenticated but role is not admin 3)If user is authenticated but role is admin 4)If user is authenticated but have no admin access and still try to access to admin page

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -3,18 +3,28 @@ import "@/styles/globals.css";
 import Head from "next/head";
 import type { AppProps } from "next/app";
 import { Provider } from "react-redux";
+import CheckAuth from "@/components/common/checkAuth";
+
 
 export default function App({ Component, pageProps }: AppProps) {
+
+const isAuthenticated = false; // Replace with actual logic to check if user is authenticated
+const user = null; 
   return (
     <Provider store={store}>
       <Head>
         <title>TrendHive</title>
 
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="description" content="One store for men,women and child" />
-        <link rel="icon" href="/logo.png" />
+        <meta
+          name="description"
+          content="One store for men, women and children"
+        />
+        <link rel="icon" href="/favicon.png" />
       </Head>
-      <Component {...pageProps} />
+      <CheckAuth isAuthenticated={isAuthenticated} user={user}>
+        <Component {...pageProps} /> {/* Render the page component */}
+      </CheckAuth>
     </Provider>
   );
 }
