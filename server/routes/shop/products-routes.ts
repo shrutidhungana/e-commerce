630510704325168c767a8b04f2effac485c93324
shrutidhungana <shrutidhungana123@gmail.com>
2024-11-17 17:41:32 +0545

    Created API for product details 1)Created controller 2)Created routes

--- server/routes/shop/products-routes.ts
+++ server/routes/shop/products-routes.ts
@@ -1,8 +1,12 @@
 import express, { Router } from "express";
-import { getFilteredProducts } from "../../controllers/shop/products-controller";
+import {
+  getFilteredProducts,
+  getProductDetails,
+} from "../../controllers/shop/products-controller";
 
 const router: Router = express.Router();
 
 router.get("/lists", getFilteredProducts as express.RequestHandler);
+router.get("/lists/:id", getProductDetails as express.RequestHandler)
 
 export default router;
