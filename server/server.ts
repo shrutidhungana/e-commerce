a31c99e0b7cde8faabacf3b591f50c3e6be75030
shrutidhungana <shrutidhungana123@gmail.com>
2024-11-22 18:19:47 +0545

    Created routes for address and imported it in server 1)Created add routes 2)fetch, edit, delete 3)Used it in main file

--- server/server.ts
+++ server/server.ts
@@ -2,18 +2,19 @@ import express from "express";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import authRouter from './routes/auth/auth-routes'
-import adminProductsRouter from './routes/admin/products-routes'
-import shopProductsRouter from './routes/shop/products-routes'
-import shopCartRouter from './routes/shop/cart-routes'
+import authRouter from "./routes/auth/auth-routes";
+import adminProductsRouter from "./routes/admin/products-routes";
+import shopProductsRouter from "./routes/shop/products-routes";
+import shopCartRouter from "./routes/shop/cart-routes";
+import shopAddressRouter from './routes/shop/address-routes'
 
-const uri:string =
+const uri: string =
   "mongodb+srv://shrutidhungana123:Falgun24@cluster0.plrct.mongodb.net/"; // Update this line
 
 mongoose
   .connect(uri)
   .then(() => console.log("MongoDB connected"))
-  .catch((err:Error) => console.error("MongoDB connection error:", err));
+  .catch((err: Error) => console.error("MongoDB connection error:", err));
 
 const app: express.Express = express();
 const PORT: number = Number(process.env.PORT) || 5000;
@@ -35,10 +36,11 @@ app.use(
 
 app.use(cookieParser());
 app.use(express.json());
-app.use('/api/auth', authRouter);
-app.use('/api/admin/products', adminProductsRouter)
+app.use("/api/auth", authRouter);
+app.use("/api/admin/products", adminProductsRouter);
 app.use("/api/shop/products", shopProductsRouter);
 app.use("/api/shop/cart", shopCartRouter);
+app.use("/api/shop/address", shopAddressRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
