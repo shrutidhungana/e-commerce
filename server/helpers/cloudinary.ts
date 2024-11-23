dba9b1822a2ac765cf08491a4887567d1715de2a
shrutidhungana <43822201+shrutidhungana@users.noreply.github.com>
2024-11-23 21:06:25 +0545

    Update cloudinary.ts

--- server/helpers/cloudinary.ts
+++ server/helpers/cloudinary.ts
@@ -4,9 +4,9 @@ import multer from "multer";
 
 // Configure Cloudinary
 cloudinary.config({
-  cloud_name: "",
-  api_key: "",
-  api_secret: "",
+  cloud_name: "dmon20gzb",
+  api_key: "593587143943919",
+  api_secret: "PMgINIzZZpNE3NEjohFHzSRqbyU",
 });
 
 const storage = multer.memoryStorage();
