017efa2591f3890e9ee55116140e20ae1a0e8972
shrutidhungana <shrutidhungana123@gmail.com>
2024-11-26 12:40:00 +0545

    Created controllers for capturePayment and orderDetails 1)Also corrected spelling

--- server/controllers/shop/order-controller.ts
+++ server/controllers/shop/order-controller.ts
@@ -20,7 +20,8 @@ const createOrder = async (
       orderDate,
       orderUpdateDate,
       paymentId,
-      payerId,
+        payerId,
+      cartId
     } = req.body;
 
     const create_payment_json = {
@@ -71,7 +72,8 @@ const createOrder = async (
           orderDate,
           orderUpdateDate,
           paymentId,
-          payerId,
+            payerId,
+          cartId
         });
 
         await newlyCreatedOrder.save();
@@ -105,6 +107,54 @@ const createOrder = async (
 
 const capturePayment = async (req: Request, res: Response) => {
   try {
+    const { paymentId, payerId, orderId } = req.body;
+
+    let order = await Order.findById(orderId);
+
+    if (!order) {
+      return res.status(404).json({
+        success: false,
+        message: "Order can not be found",
+      });
+    }
+
+    order.paymentStatus = "paid";
+    order.orderStatus = "confirmed";
+    order.paymentId = paymentId;
+    order.payerId = payerId;
+
+    for (let item of order.cartItems) {
+      let product = await Product.findById(item.productId);
+
+      if (!product) {
+        return res.status(404).json({
+          success: false,
+          message: `Product not found for item with ID: ${item.productId}`,
+        });
+      }
+
+        if (product.totalStock < item.quantity) {
+          return res.status(400).json({
+            success: false,
+            message: `Not enough stock for the product: ${product.title}`,
+          });
+        } 
+        
+      product.totalStock -= item.quantity;
+
+      await product.save();
+    }
+
+    const getCartId = order.cartId;
+    await Cart.findByIdAndDelete(getCartId);
+
+    await order.save();
+
+    res.status(200).json({
+      success: true,
+      message: "Order confirmed",
+      data: order,
+    });
   } catch (e) {
     res.status(500).json({
       success: false,
@@ -115,8 +165,23 @@ const capturePayment = async (req: Request, res: Response) => {
 
 
 
-const getALlOrderByUser = async (req: Request, res: Response) => {
+const getAllOrdersByUser = async (req: Request, res: Response) => {
   try {
+    const { userId } = req.params;
+
+    const orders = await Order.find({ userId });
+
+    if (!orders.length) {
+      return res.status(404).json({
+        success: false,
+        message: "No orders found!",
+      });
+    }
+
+    res.status(200).json({
+      success: true,
+      data: orders,
+    });
   } catch (e) {
     res.status(500).json({
       success: false,
@@ -135,4 +200,4 @@ const getOrderDetails = async (req: Request, res: Response) => {
   }
 };
 
-export { createOrder, capturePayment, getALlOrderByUser, getOrderDetails };
+export { createOrder, capturePayment, getAllOrdersByUser, getOrderDetails };
