599c2d7fcc376940bda08645f0b8996071209f39
shrutidhungana <shrutidhungana123@gmail.com>
2024-11-26 20:40:16 +0545

    Created slices for order-list and order details 1)Also added the add cases for them

--- client/src/store/shop/order-slice/index.ts
+++ client/src/store/shop/order-slice/index.ts
@@ -1,7 +1,13 @@
 import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
 import axios from "axios";
 import { apiEndpoints } from "@/utils/api";
-import { InitialOrderState, Capture, OrderPayload, CapturePaymentParams } from "@/types";
+import {
+  InitialOrderState,
+  Capture,
+  OrderPayload,
+  CapturePaymentParams,
+  Order,
+} from "@/types";
 
 const initialState: InitialOrderState = {
   approvalURL: null,
@@ -11,8 +17,7 @@ const initialState: InitialOrderState = {
   orderDetails: null,
 };
 
-const { createOrder, captureAPayment, getOrderDetails, getOrderList } =
-  apiEndpoints;
+const { createOrder, captureAPayment, OrderDetails, OrderList } = apiEndpoints;
 
 export const createNewOrder = createAsyncThunk<InitialOrderState, OrderPayload>(
   "/order/createNewOrder",
@@ -30,6 +35,22 @@ export const capturePayment = createAsyncThunk<Capture, CapturePaymentParams>(
       payerId,
       orderId,
     });
+    return response.data;
+  }
+);
+
+export const getAllOrdersByUserId = createAsyncThunk<
+  { data: Array<Order> },
+  string
+>("/order/getAllOrdersByUserId", async (userId) => {
+  const response = await axios.get(`${OrderList?.replace(":userId", userId)}`);
+  return response?.data;
+});
+
+export const getOrderDetails = createAsyncThunk<{ data: Order }, string>(
+  "/order/getOrderDetails",
+  async (id) => {
+    const response = await axios.get(`${OrderDetails?.replace("id", id)}`);
     return response.data;
   }
 );
@@ -59,6 +80,34 @@ const shoppingOrderSlice = createSlice({
         state.isLoading = false;
         state.approvalURL = null;
         state.orderId = null;
+      })
+      .addCase(getAllOrdersByUserId.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(
+        getAllOrdersByUserId.fulfilled,
+        (state, action: PayloadAction<{ data: Array<Order> }>) => {
+          state.isLoading = false;
+          state.orderList = action.payload.data;
+        }
+      )
+      .addCase(getAllOrdersByUserId.rejected, (state) => {
+        state.isLoading = false;
+        state.orderList = [];
+      })
+      .addCase(getOrderDetails.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(
+        getOrderDetails.fulfilled,
+        (state, action: PayloadAction<{ data: Order }>) => {
+          state.isLoading = false;
+          state.orderDetails = action.payload.data;
+        }
+      )
+      .addCase(getOrderDetails.rejected, (state) => {
+        state.isLoading = false;
+        state.orderDetails = null;
       });
   },
 });
