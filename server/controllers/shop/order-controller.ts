c313c9985c6dbb365a63b7e3aebb98d5c38bf0fa
shrutidhungana <shrutidhungana123@gmail.com>
2024-11-26 12:42:50 +0545

    Created controllers for get order details 1)All the orders

--- server/controllers/shop/order-controller.ts
+++ server/controllers/shop/order-controller.ts
@@ -191,13 +191,28 @@ const getAllOrdersByUser = async (req: Request, res: Response) => {
 };
 
 const getOrderDetails = async (req: Request, res: Response) => {
-  try {
-  } catch (e) {
-    res.status(500).json({
-      success: false,
-      message: "Some error occured!",
-    });
-  }
+ try {
+   const { id } = req.params;
+
+   const order = await Order.findById(id);
+
+   if (!order) {
+     return res.status(404).json({
+       success: false,
+       message: "Order not found!",
+     });
+   }
+
+   res.status(200).json({
+     success: true,
+     data: order,
+   });
+ } catch (e) {
+   res.status(500).json({
+     success: false,
+     message: "Some error occured!",
+   });
+ }
 };
 
 export { createOrder, capturePayment, getAllOrdersByUser, getOrderDetails };
