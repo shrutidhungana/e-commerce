d6b830846217636094b665802026d25a0227ac15
shrutidhungana <shrutidhungana123@gmail.com>
2024-11-27 22:02:10 +0545

    Used the routes created for review functionality in main server page 1)Used using app.use method

--- server/server.ts
+++ server/server.ts
@@ -3,13 +3,16 @@ import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import authRouter from "./routes/auth/auth-routes";
+
 import adminProductsRouter from "./routes/admin/products-routes";
-import adminOrderRouter from './routes/admin/order-routes';
+import adminOrderRouter from "./routes/admin/order-routes";
+
 import shopProductsRouter from "./routes/shop/products-routes";
 import shopCartRouter from "./routes/shop/cart-routes";
 import shopAddressRouter from "./routes/shop/address-routes";
-import shopOrderRouter from "./routes/shop/order-routes"
-import shopSearchRouter from './routes/shop/search-routes'
+import shopOrderRouter from "./routes/shop/order-routes";
+import shopSearchRouter from "./routes/shop/search-routes";
+import shopReviewRouter from "./routes/shop/review-routes";
 
 const uri: string =
   "mongodb+srv://shrutidhungana123:Falgun24@cluster0.plrct.mongodb.net/"; // Update this line
@@ -40,13 +43,16 @@ app.use(
 app.use(cookieParser());
 app.use(express.json());
 app.use("/api/auth", authRouter);
+
 app.use("/api/admin/products", adminProductsRouter);
 app.use("/api/admin/orders", adminOrderRouter);
+
 app.use("/api/shop/products", shopProductsRouter);
 app.use("/api/shop/cart", shopCartRouter);
 app.use("/api/shop/address", shopAddressRouter);
 app.use("/api/shop/order", shopOrderRouter);
 app.use("/api/shop/search", shopSearchRouter);
+app.use("/api/shop/review", shopReviewRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
