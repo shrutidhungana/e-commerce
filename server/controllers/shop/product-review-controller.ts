7037b195b594ed7819288b52f575348513d2dea6
shrutidhungana <shrutidhungana123@gmail.com>
2024-11-27 21:45:53 +0545

    Created controller for addProductReview 1)Basically writing review

---
+++ server/controllers/shop/product-review-controller.ts
@@ -0,0 +1,70 @@
+import Order from "../../modals/Order";
+import Product from "../../modals/Product";
+import ProductReview from "../../modals/Review";
+import { Request, Response } from "express";
+
+const addProductReview = async (
+  req: Request,
+  res: Response
+): Promise<void | Response> => {
+  try {
+    const { productId, userId, userName, reviewMessage, reviewValue } =
+      req.body;
+
+    const order = await Order.findOne({
+      userId,
+      "cartItems.productId": productId,
+      // orderStatus: "confirmed" || "delivered",
+    });
+
+    if (!order) {
+      return res.status(403).json({
+        success: false,
+        message: "You need to purchase product to review it.",
+      });
+    }
+
+    const checkExistinfReview = await ProductReview.findOne({
+      productId,
+      userId,
+    });
+
+    if (checkExistinfReview) {
+      return res.status(400).json({
+        success: false,
+        message: "You already reviewed this product!",
+      });
+    }
+
+    const newReview = new ProductReview({
+      productId,
+      userId,
+      userName,
+      reviewMessage,
+      reviewValue,
+    });
+
+    await newReview.save();
+
+    const reviews = await ProductReview.find({ productId });
+    const totalReviewsLength = reviews.length;
+    const averageReview =
+      reviews.reduce((sum, reviewItem) => sum + reviewItem.reviewValue, 0) /
+      totalReviewsLength;
+
+    await Product.findByIdAndUpdate(productId, { averageReview });
+
+    res.status(201).json({
+      success: true,
+      data: newReview,
+    });
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Error",
+    });
+  }
+};
+
+export { addProductReview };
