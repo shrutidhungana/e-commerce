450ccaaf05c7a45f92ec06713626b244ed361dd4
shrutidhungana <shrutidhungana123@gmail.com>
2024-12-12 20:27:11 +0545

    updated package.json

--- server/server.ts
+++ server/server.ts
@@ -29,7 +29,7 @@ const PORT: number = Number(process.env.PORT) || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "http://localhost:3001"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
